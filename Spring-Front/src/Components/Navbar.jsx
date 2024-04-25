7a84a0696b83b23dcdf08f897e83589d217c804e
FraNkoRasia <rasiafranko@gmail.com>
2024-04-24 22:09:59 -0300

    menu hamburguesa

--- Spring-Front/src/Components/Navbar.jsx
+++ Spring-Front/src/Components/Navbar.jsx
@@ -1,51 +1,64 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngleUp, faAngleDown } from '@fortawesome/free-solid-svg-icons'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleUp, faAngleDown } from '@fortawesome/free-solid-svg-icons';
 
 export default function Navbar({ user, setUser }) {
-    const [isProfileDropdownOpen, setIsProfileDropdownOpen] = useState(false); // Estado para visibilidad desplegable
+    const [isProfileDropdownOpen, setIsProfileDropdownOpen] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
 
     useEffect(() => {
-        const closeProfileDropdown = () => {
-            setIsProfileDropdownOpen(false);
+        const closeDropdowns = (event) => {
+            if (!event.target.closest('#profile')) {
+                setIsProfileDropdownOpen(false);
+            }
+            if (!event.target.closest('.abrir-menu') && !event.target.closest('.nav')) {
+                setMenuOpen(false);
+            }
         };
 
-        // Cerrar el menú desplegable del perfil al hacer clic fuera de él
-        document.addEventListener('click', closeProfileDropdown);
+        document.addEventListener('click', closeDropdowns);
 
-        // Limpiar el detector de eventos al desmontar
         return () => {
-            document.removeEventListener('click', closeProfileDropdown);
+            document.removeEventListener('click', closeDropdowns);
         };
     }, []);
 
-    const handleProfileClick = (event) => {
-        event.stopPropagation(); // Evitar la propagación de eventos al detector de clics en el documento
-        setIsProfileDropdownOpen(!isProfileDropdownOpen); // Alternar visibilidad del menú desplegable al hacer clic
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
     };
 
-    // console.log("Valor actual de user en Navbar:", user); // Registrar información del usuario
-    // console.log(JSON.stringify(user));
+    const handleProfileClick = (event) => {
+        event.stopPropagation();
+        setIsProfileDropdownOpen(!isProfileDropdownOpen);
+    };
+
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        setUser(null);
+    };
 
     if (user === null) {
         // Representar contenido para usuarios no autenticados (sin cambios)
         return (
-            <header>
-                <div className='flex align-center'>
-                    <Link className='titPlay' to="/">
-                        <p>PLAY CODE</p>
-                    </Link>
-                    <nav>
-                        <ul className='flex nav-1'>
-                            <li><Link to="/login" className="sin-subrayado">LOGIN</Link></li>
-                            <li><Link to="/registro" className="sin-subrayado">REGISTRO</Link></li>
-                            <li><Link to="/contacto" className="sin-subrayado">CONTACTO</Link></li>
-                        </ul>
+            <header className="header">
 
-                    </nav>
-                </div>
+                <Link className='titPlay' to="/">
+                    <p>PLAY CODE</p>
+                </Link>
+                <button id="abrir" className="abrir-menu" onClick={toggleMenu}>
+                    {menuOpen ? <i className="bi bi-x"></i> : <i className="bi bi-list"></i>}
+                </button>
+                <nav id="nav" className={`nav ${menuOpen ? "visible" : ""}`}>
+                    <button className="cerrar-menu" id="cerrar" onClick={toggleMenu}>
+                        <i className="bi bi-x"></i>
+                    </button>
+                    <ul className="nav-list">
+                        <li><Link to="/login" className="nav-list-color">LOGIN</Link></li>
+                        <li><Link to="/registro" className="nav-list-color">REGISTRO</Link></li>
+                        <li><Link to="/contacto" className="nav-list-color">CONTACTO</Link></li>
+                    </ul>
+                </nav>
 
             </header>
         );
@@ -57,48 +70,54 @@ export default function Navbar({ user, setUser }) {
         };
 
         return (
-            <header>
-                <div className='flex align-center celu-responsive'>
-                    <Link className='titPlay' to="/">
-                        <p>PLAY CODE</p>
-                    </Link>
+            <header className="header">
 
-                    <nav>
-                        <ul className='flex nav-2'>
+                <Link className='titPlay' to="/">
+                    <p>PLAY CODE</p>
+                </Link>
+                <button id="abrir" className="abrir-menu" onClick={toggleMenu}>
+                    {menuOpen ? <i className="bi bi-x"></i> : <i className="bi bi-list"></i>}
+                </button>
+                <nav id="nav" className={`nav ${menuOpen ? "visible" : ""}`}>
+                    <button className="cerrar-menu" id="cerrar" onClick={toggleMenu}>
+                        <i className="bi bi-x"></i>
+                    </button>
+                    <ul className="nav-list">
 
-                            <li>
-                                <Link to="/dashboard" className="sin-subrayado">PANEL</Link>
-                            </li>
+                        <li>
+                            <Link to="/dashboard" className="nav-list-color">PANEL</Link>
+                        </li>
 
-                            {user.role === 'ADMINISTRATOR' && ( // Representación condicional según el rol del usuario
-                                <li><Link to="/lista-usuarios" className="sin-subrayado">USUARIOS</Link></li>
+                        {user.role === 'ADMINISTRATOR' && ( // Representación condicional según el rol del usuario
+                            <li><Link to="/lista-usuarios" className="nav-list-color">USUARIOS</Link></li>
+                        )}
+
+                        <li className="dropdown-container"> {/* Envolver "Perfil" en un contenedor */}
+                            <Link id="profile" className="nav-list-color" onClick={handleProfileClick}>
+                                PERFIL {isProfileDropdownOpen ? <FontAwesomeIcon icon={faAngleUp} /> : <FontAwesomeIcon icon={faAngleDown} />}
+                            </Link>
+
+                            {isProfileDropdownOpen && ( // Representar condicionalmente el contenido desplegable
+                                <ul className="dropdown-menu">
+                                    <li><Link to="/perfil" className="nav-list-color" >Modificar Perfil</Link></li>
+                                    <li><Link to="/editpassword" className="nav-list-color" >Modificar Contraseña</Link></li>
+                                </ul>
                             )}
+                        </li>
 
-                            <li className="dropdown-container"> {/* Envolver "Perfil" en un contenedor */}
-                                <Link id="profile" className="sin-subrayado" onClick={handleProfileClick}>
-                                    PERFIL {isProfileDropdownOpen ? <FontAwesomeIcon icon={faAngleUp} /> : <FontAwesomeIcon icon={faAngleDown} />}
-                                </Link>
+                        <li><Link to="/contacto" className="nav-list-color">CONTACTO</Link></li>
+                        <li className="cerrarSesionContainer">
+                            <button className="cerrarSesion" onClick={handleLogout}>
+                                Cerrar Sesión
+                            </button>
+                        </li>
+                    </ul>
 
-                                {isProfileDropdownOpen && ( // Representar condicionalmente el contenido desplegable
-                                    <ul className="dropdown-menu">
-                                        <li><Link to="/perfil" className="sin-subrayado" >Modificar Perfil</Link></li>
-                                        <li><Link to="/editpassword" className="sin-subrayado" >Modificar Contraseña</Link></li>
-                                    </ul>
-                                )}
-                            </li>
+                </nav>
 
-                            <li><Link to="/contacto" className="sin-subrayado">CONTACTO</Link></li>
-                        </ul>
 
-                    </nav>
-                    <li className="cerrarSesionContainer">
-                        <button className="cerrarSesion" onClick={handleLogout}>
-                            Cerrar Sesión
-                        </button>
-                    </li>
 
-                </div>
-            </header>
+            </header >
         );
     }
 }
