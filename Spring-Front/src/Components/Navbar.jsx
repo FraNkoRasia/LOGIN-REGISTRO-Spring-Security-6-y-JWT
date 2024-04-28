b969d59699973afb5a7734d6ae2f376c40f1ab2d
FraNkoRasia <rasiafranko@gmail.com>
2024-04-28 12:06:08 -0300

    agrego iconos al navbar

--- Spring-Front/src/Components/Navbar.jsx
+++ Spring-Front/src/Components/Navbar.jsx
@@ -2,6 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleUp, faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import { faHouseChimney } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faIdBadge } from '@fortawesome/free-solid-svg-icons';
+import { faMessage } from '@fortawesome/free-solid-svg-icons';
+import { faRightToBracket } from '@fortawesome/free-solid-svg-icons';
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+
+
 
 export default function Navbar({ user, setUser }) {
     const [isProfileDropdownOpen, setIsProfileDropdownOpen] = useState(false);
@@ -60,9 +68,27 @@ export default function Navbar({ user, setUser }) {
                                 <p className='titPlayHamburguesa'>PLAY CODE</p>
                             </Link>
                         </li>
-                        <li><Link to="/login" className="nav-list-color">LOGIN</Link></li>
-                        <li><Link to="/registro" className="nav-list-color">REGISTRO</Link></li>
-                        <li><Link to="/contacto" className="nav-list-color">CONTACTO</Link></li>
+                        <li><Link to="/login" className="nav-list-color">
+                            <span className='iconoLogin'>
+                                <FontAwesomeIcon icon={faRightToBracket} style={{ color: "#ffffff", }} />
+                            </span>
+                            LOGIN</Link></li>
+                        <li>
+                            <Link to="/registro" className="nav-list-color">
+                                <span className='iconoRegistro'>
+                                    <FontAwesomeIcon icon={faPenToSquare} style={{ color: "#ffffff", }} />
+                                </span>
+                                REGISTRO
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/contacto" className="nav-list-color">
+                                <span className='iconoContacto'>
+                                    <FontAwesomeIcon icon={faMessage} style={{ color: "#ffffff", }} />
+                                </span>
+                                CONTACTO
+                            </Link>
+                        </li>
                     </ul>
                 </nav>
 
@@ -94,27 +120,49 @@ export default function Navbar({ user, setUser }) {
                             </Link>
                         </li>
                         <li>
-                            <Link to="/dashboard" className="nav-list-color">PANEL</Link>
+                            <Link to="/dashboard" className="nav-list-color">
+                                <span className='iconoPanel'>
+                                    <FontAwesomeIcon icon={faHouseChimney} /> {/* Agrega el icono aquí */}
+                                </span>
+                                PANEL
+                            </Link>
                         </li>
 
                         {user.role === 'ADMINISTRATOR' && ( // Representación condicional según el rol del usuario
-                            <li><Link to="/lista-usuarios" className="nav-list-color">USUARIOS</Link></li>
+                            <li>
+                                <Link to="/dashboard" className="nav-list-color">
+                                    <span className='iconoUsuario'>
+                                        <FontAwesomeIcon icon={faUser} style={{ color: "#ffffff" }} /> {/* Usa el icono faUser */}
+                                    </span>
+                                    USUARIO
+                                </Link>
+                            </li>
                         )}
 
-                        <li className="dropdown-container"> {/* Envolver "Perfil" en un contenedor */}
+                        <li className="dropdown-container">
                             <Link id="profile" className="nav-list-color" onClick={handleProfileClick}>
+                                <span className='iconoPerfil'>
+                                    <FontAwesomeIcon icon={faIdBadge} size="lg" style={{ color: "#ffffff" }} />
+                                </span>
                                 PERFIL {isProfileDropdownOpen ? <FontAwesomeIcon icon={faAngleUp} /> : <FontAwesomeIcon icon={faAngleDown} />}
                             </Link>
+                            {/* Resto del contenido desplegable */}
+
 
                             {isProfileDropdownOpen && ( // Representar condicionalmente el contenido desplegable
                                 <ul className="dropdown-menu">
-                                    <li><Link to="/perfil" className="nav-list-color" >Modificar Perfil</Link></li>
+                                    <li>
+                                        <Link to="/perfil" className="nav-list-color" >Modificar Perfil</Link></li>
                                     <li><Link to="/editpassword" className="nav-list-color" >Modificar Contraseña</Link></li>
                                 </ul>
                             )}
                         </li>
 
-                        <li><Link to="/contacto" className="nav-list-color">CONTACTO</Link></li>
+                        <li><Link to="/contacto" className="nav-list-color">
+                            <span className='iconoContacto'>
+                                <FontAwesomeIcon icon={faMessage} style={{ color: "#ffffff", }} />
+                            </span>
+                            CONTACTO</Link></li>
                         <li>
                             <button className='cerrarSesion' onClick={handleLogout}>
                                 Cerrar Sesión
